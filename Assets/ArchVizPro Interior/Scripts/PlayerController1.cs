d681993e942db84850c0dbe609cc490895d0bab7
Sami Palomäki <sami.palomaki@hotmail.com>
2023-09-06 18:22:51 +0300

    nimetty uudelleen toinen PlayerController.cs

--- Assets/ArchVizPro Interior/Scripts/PlayerController1.cs
+++ Assets/ArchVizPro Interior/Scripts/PlayerController1.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEditor;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController1 : MonoBehaviour
 {
     PlayerInput Controller;
     public GameObject TextObject;
