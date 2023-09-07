cba8f4c4d6ba1f6fd08770d5c882ee64b6f7890b
mordeni <eskojee123@gmail.com>
2023-09-07 12:11:12 +0300

    parempi kauppa ja hangar

--- Assets/Scripts/Inventory/InventoryItems.cs
+++ Assets/Scripts/Inventory/InventoryItems.cs
@@ -25,6 +25,8 @@ public class InventoryItems : MonoBehaviour
     public static int pistol = 0;
 
     public static int shotgun = 0;
+
+    public static int gold = 300;
     // Start is called before the first frame update
     void Start()
     {
